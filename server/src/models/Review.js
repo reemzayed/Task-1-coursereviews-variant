255f1bad4aabba4ba0ad10f3f3cfe80271856aab
reemzayed <reemzaied07@gmail.com>
2026-09-19 11:38:37 +0300

    docs: feat: implement review CRUD and course summary API

--- server/src/models/Review.js
+++ server/src/models/Review.js
@@ -5,10 +5,31 @@ import mongoose from 'mongoose';
 const reviewSchema = new mongoose.Schema(
   {
     // TODO
+    courseCode: { 
+      type: String, 
+      required: true 
+    },
+    rating: { 
+      type: Number, 
+      required: true, 
+      min: 1, 
+      max: 5 
+    },
+    comment: { 
+      type: String,
+    }, //optional so no need to include required: false because it is false by default
+    reviewedBy: { 
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: 'User'
+    } //optional 
   },
   { timestamps: true }
-);
 
+);
 // TODO: add the uniqueness constraint described in README.md section 1.
+reviewSchema.index(
+      { courseCode: 1, reviewedBy: 1 }, 
+      { unique: true }
+);
 
 export const Review = mongoose.model('Review', reviewSchema);
